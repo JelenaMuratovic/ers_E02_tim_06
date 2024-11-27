9f6dbb82d0e4ba0e3925ea5f351319c5fe2f85a6
NevenaGatalo <nevena.gatalo@gmail.com>
2024-11-27 10:55:34 +0100

    kreiranje klase ruter

---
+++ Domain/Models/Ruter.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ruter
+    {
+        public string SerijskiBrojProizvodjaca { get; set; } = "";
+        public int MaxBrzinaPrenosaPodataka { get; set; }
+        public int BrojLANPrikljucka { get; set; }
+        public VrstaRutera VrstaRutera { get; set; }
+    }
+}
