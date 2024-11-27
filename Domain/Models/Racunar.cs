02f5a65e670cd4b9ac83f332dc9e6f9694fc157e
JelenaMuratovic <jelena.muratovic3@gmail.com>
2024-11-27 10:45:14 +0100

    kreiranje klase racunar

---
+++ Domain/Models/Racunar.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Racunar
+    {
+        public string SerijskiBrojProizvodjaca { get; set; } = "";
+        public int KapacitetRadneMemorije { get; set; }
+        public int KapacitetSkladisneMemorije { get; set; }
+        public TipSkladisneMemorije TipSkladisneMemorije { get; set; }
+        public string LokalnaIPAdresa { get; set; } = "";
+    }
+}
