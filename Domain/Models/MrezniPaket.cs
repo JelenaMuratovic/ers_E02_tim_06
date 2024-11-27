c3aa14fc4f8f1536cf608c184ba8de1752530479
NevenaGatalo <nevena.gatalo@gmail.com>
2024-11-27 11:05:16 +0100

    kreiranje klase MrezniPaket

---
+++ Domain/Models/MrezniPaket.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class MrezniPaket
+    {
+        public MrezniProtokol Protokol { get; set; }
+        public int VelicinaZaglavlja { get; set; }
+        public int VelicinaDelaSaPodacima { get; set; }
+        public string Sadrzaj { get; set; } = "";
+        public string IPAdresaPrimaoca { get; set; } = "";
+    }
+}
